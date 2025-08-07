0851b894bbec3a07b3707e98cc16c917a2c796dd
ThiagoKagoiki <thiagoyck@gmail.com>
2025-08-07 11:31:17 -0300

    Frontend sem CSS

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 
 export const Home = () => {
-    
+
+    let urlEncurtada
+
+    return (
+        <div>
+            <form action="">
+                <input type="text" placeholder="Código desejado" />
+                <input type="text" placeholder="URL" />
+                <button>Encurtar</button>
+            </form>
+            <span>{urlEncurtada}</span>
+        </div>
+    )
 }
