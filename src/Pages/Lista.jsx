53f50224cf7c21581f4ac27b6ad49a1a21a0b0df
ThiagoKagoiki <thiagoyck@gmail.com>
2025-08-08 00:00:18 -0300

    Falta apenas o estilo

--- src/Pages/Lista.jsx
+++ src/Pages/Lista.jsx
@@ -1,11 +1,40 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { pegarUrl } from "../services/service";
 
 export const Lista = () => {
-    const [codigo, setCodigo] = useState('')
-    const [url, setUrl] = useState('')
-    return(
-        <>
+    const [list, setList] = useState([])
 
+    const verLista = async () => {
+        try {
+            const resposta = await pegarUrl()
+            setList(resposta.data)
+        } catch (error) {
+            console.error("erro ao carregar lista: ", error);
+        }
+    }
+
+    useEffect(() => {
+        verLista()
+    })
+
+    return (
+        <>
+            <div className="grid">
+                {list.map((lista) => (
+                    <div className="consulta-item" key={lista.id}>
+                        <div className="consulta-item-header">
+                            <div className="consulta-item-info">
+                                <h3 className="consulta-item-title">N°: {lista.id}</h3>
+                                <div className="consulta-item-details">
+                                    <p><span className="font-medium">Código:</span> {lista.codigo}</p>
+                                    <p><span className="font-medium">Url:</span> {lista.urlOriginal}</p>
+                                    <p><span className="font-medium">ShortLink:</span> {lista.shortLink}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </>
     )
 }
